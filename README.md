9ca934d41bc8ef7c8556bb4660e22066d7b25a1b
Olivier Filangi <olivier.filangi@gmail.com>
2021-01-18 13:16:48 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# services
+
+service available at [genouest/openstack](https://p2m2-192-168-100-248.vm.openstack.genouest.org/#)
+
+
+
---
+++ endpoints.js
@@ -0,0 +1,9 @@
+var endpoints = [
+  {
+    "name": "MetaboRef",
+    "url": "https://p2m2-192-168-100-248.vm.openstack.genouest.org/sparql"
+  },
+  {
+    "name": "BBIP - Brassicaceae BioInformatics Platform",
+    "url": "https://cors-anywhere.herokuapp.com/https://bbip.askomics.org/virtuoso/sparql"
+  }]
