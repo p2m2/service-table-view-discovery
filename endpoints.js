f7e230a2d17e0443937760e3d8e8327843469d02
Olivier Filangi <olivier.filangi@gmail.com>
2021-01-19 16:01:03 +0100

    add peakforest clermont

--- endpoints.js
+++ endpoints.js
@@ -4,6 +4,10 @@ var endpoints = [
     "url": "https://p2m2-192-168-100-248.vm.openstack.genouest.org/metaboref/sparql"
   },
   {
-    "name": "BBIP - Brassicaceae BioInformatics Platform",
+    "name" : "PeakForest MetaboHUB Clermont",
+    "url" : "https://peakforest.semantic-metabolomics.fr/sparql"
+  },
+  {
+    "name": "Brassicaceae BioInformatics Platform",
     "url": "https://cors-anywhere.herokuapp.com/https://bbip.askomics.org/virtuoso/sparql"
   }]
